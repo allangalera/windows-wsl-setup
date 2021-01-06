99c684126183886944099fd9f531cbf6405a563c
Allan Galera <allangalera@gmail.com>
2021-01-05 21:50:29 -0300

    add yarn installation

--- setup.sh
+++ setup.sh
@@ -30,6 +30,12 @@ nvm install 'lts/*'
 nvm install node
 nvm alias default 'lts/*'
 
+# install yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update -y
+sudo apt install -y --no-install-recommends yarn
+
 # install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
