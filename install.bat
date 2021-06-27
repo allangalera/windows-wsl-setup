e266fb59b8490a3dea58ab687f8f886c5d792d9b
Allan Galera <allangalera@gmail.com>
2021-06-27 20:46:41 -0300

    change from choco to winget and add vscode extensions

--- install.bat
+++ install.bat
@@ -1,36 +1,59 @@
 @echo off
 
-powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+@REM my default font for vscode and terminal
+@REM https://github.com/microsoft/cascadia-code
+@REM default font with icons
+@REM https://github.com/ryanoasis/nerd-fonts/tree/master/patched-fonts/CascadiaCode
 
-choco feature enable -n=allowGlobalConfirmation
+@REM enable WSL
+powershell dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+@REM enable virtual machine
+powershell dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 
-choco install googlechrome
-choco pin add -n=googlechrome
+winget install VideoLAN.VLC
+winget install Bitwarden.Bitwarden
+winget install Mozilla.Firefox
+winget install Notepad++.Notepad++
+winget install Microsoft.PowerToys
+winget install Git.Git
+@REM maybe change to other terminal
+winget install Microsoft.WindowsTerminal
+winget install Eugeny.Terminus
+winget install 7zip.7zip
+winget install voidtools.Everything
+@REM tool to have GUI on WSL remove when is natively available
+winget install marha.VcXsrv
+winget install Discord.Discord
+winget install Spotify.Spotify
+winget install Telegram.TelegramDesktop
+winget install WhatsApp.WhatsApp
+winget install SlackTechnologies.Slack
+winget install Valve.Steam
+winget install Twilio.Authy
+winget install Bitwarden.Bitwarden
+winget install OpenVPNTechnologies.OpenVPN
+winget install BraveSoftware.BraveBrowser
+winget install Microsoft.VisualStudioCode
+winget install plex.plexmediaserver
+winget install qBittorrent.qBittorrent
+winget install rammichael.7+TaskbarTweaker
+winget install GitHub.cli
 
-choco install notepadplusplus.install
-choco install powertoys
-
-choco install git.install -y --params "/GitAndUnixToolsOnPath /NoAutoCrlf /WindowsTerminal"
-call refreshenv
-rem always have Linux line endings in text files
-git config --global core.autocrlf input
-git config --global core.longpaths true
-
-choco install powershell-core
-choco install vcxsrv
-choco install everything
-choco install 7zip
-choco install vlc
-choco install discord
-choco install spotify
-choco install telegram
-choco install whatsapp
-choco install slack
-choco install steam
-choco install authy-desktop
-choco install bitwarden
-choco install openvpn
-choco install brave
-
-choco install vscode
-choco pin add -n vscode
+@REM vscode extensions
+code --install-extension dracula-theme.theme-dracula
+code --install-extension eamodio.gitlens
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension esbenp.prettier-vscode
+code --install-extension formulahendry.auto-close-tag
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension wmaurer.change-case
+code --install-extension aaron-bond.better-comments
+code --install-extension vscode-icons-team.vscode-icons
+code --install-extension Shan.code-settings-sync
+code --install-extension wix.vscode-import-cost
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension wwm.better-align
+code --install-extension pnp.polacode
+code --install-extension oderwat.indent-rainbow
+code --install-extension bmalehorn.vscode-fish
+code --install-extension chakrounanas.turbo-console-log
--- setup.sh
+++ setup.sh
@@ -14,9 +14,9 @@ git config --global init.defaultBranch main
 # dependencies to x-server
 # this is used to open GUI applications like gitkraken :)
 # follow this tutorial https://nickymeuleman.netlify.app/blog/gui-on-wsl2-cypress
-sudo apt install-y libgtk-3-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2
+sudo apt install -y libgtk-3-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2
 # add permission to dbus to run without password
-echo (whoami)" ALL = (root) NOPASSWD: /etc/init.d/dbus" | sudo tee -a /etc/sudoers.d/dbus
+echo $(whoami)" ALL = (root) NOPASSWD: /etc/init.d/dbus" | sudo tee -a /etc/sudoers.d/dbus
 # make sure you have https://sourceforge.net/projects/vcxsrv/ installed
 # when running use "start as client" > and select "disable access control"
 
@@ -32,52 +32,39 @@ sudo apt install -y jq      # sed json files
 sudo apt install -y ripgrep # better grep
 sudo apt install -y tree    # see folder tree structure
 
-# install nvm, altest stable and current node and set stable as default
-nvm_version=$(curl --silent "https://api.github.com/repos/nvm-sh/nvm/releases/latest" | grep "tag_name" | sed -E 's/.*"([^"]+)".*/\1/')
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/$nvm_version/install.sh | bash
-nvm install 'lts/*'
-nvm install node
-nvm alias default 'lts/*'
-
-# install yarn
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt update -y
-sudo apt install -y --no-install-recommends yarn
+# install volta instead of nvm
+curl https://get.volta.sh | bash
+volta install node
+volta install yarn
 
 # install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 # install fish shell, set as default, download config file
-sudo apt install fish
+sudo apt install fish -y
 chsh -s /usr/bin/fish
 mkdir -p ~/.config/fish
 curl -o ~/.config/fish/config.fish https://raw.githubusercontent.com/allangalera/wsl-initial-setup/main/config.fish
 
 # install oh-my-fish and theme
 curl -L https://get.oh-my.fish | fish
-omf theme agnoster
-
+omf install agnoster
+# 
 # install fisher and extensions
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-fisher add FabioAntunes/fish-nvm # port nvm to fish
-fisher add edc/bass              # Bass makes it easy to use utilities written for Bash in fish shell.
+fisher install edc/bass              # Bass makes it easy to use utilities written for Bash in fish shell.
 fisher install jorgebucaran/autopair.fish
-
-
-# install gitkraken (git GUI application)
-wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
-sudo dpkg -i gitkraken-amd64.deb
-sudo apt install -f
-rm -rf gitkraken-amd64.deb
 
 # install rust cargo packages
 cargo install lsd # prettier ls
 cargo install rm-improved # safe rm
-cargo install gitui # guit ui for the terminal
 cargo install bottom # better than top (in my opinion)
-
-# TODO: install vs-code extensions
+cargo install diskonaut # disk space visualizer
+cargo install broot # tree file explorer
+cargo install fd-find # better find
+cargo install hyperfine # benchmarking tool.
+cargo install gping # Ping, but with a graph.
+cargo install dprint # faster prittier
 
 # uninstall
 sudo apt -y autoremove
